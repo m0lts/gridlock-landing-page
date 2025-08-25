b797801d60746355b4d5919290544b7a148b15ac
Fiifi Arkhurst Jnr <arkhurst.net@gmail.com>
2025-08-25 07:47:47 +0000

    Add Success component route to App for post-payment confirmation

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Hero } from "./components/Hero";
 import { Routes, Route } from "react-router-dom";
 import Payment from "./components/Payment";
 import "./assets/styles.css";
+import Success from "./components/Success";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Hero />} />
         <Route path="/payment" element={<Payment />} />
+        <Route path="/success" element={<Success />} />
       </Routes>
     </>
   );
